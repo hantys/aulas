6643855ad2249361758060c8de9734954ce8efaf
Pedro Fausto <pedro.fausto@hotmail.com>
2018-09-19 11:14:46 -0300

    exercicios modulo 1 es6

--- curso-es6/bundle.js
+++ curso-es6/bundle.js
@@ -1,72 +1,46 @@
 "use strict";
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var List =
-/*#__PURE__*/
-function () {
-  function List() {
-    _classCallCheck(this, List);
-
-    this.data = [];
+// 3.1
+// const arr = [1, 2, 3, 4, 5];
+// const teste = arr.map(item => item + 10);
+// console.log(teste);
+// 3.2 
+// Dica: Utilize uma constante pra function
+// const usuario = { nome: 'Diego', idade: 23 };
+// const mostraIdade = () => console.log(usuario.idade)
+// mostraIdade(usuario);
+// 3.3
+// Dica: Utilize uma constante pra function
+// const nome = "pedro";
+// const idade = 23;
+// const mostraUsuario = (nome = 'Diego', idade = 18) => (console.log({ nome, idade }));
+// mostraUsuario(nome, idade);
+// mostraUsuario(nome);
+// 3.4
+// const promise = () => new Promise( (resolve, reject) => resolve())
+// console.log(promise);
+var empresa = {
+  nome: 'Rocketseat',
+  endereco: {
+    cidade: 'Rio do Sul',
+    estado: 'SC'
   }
-
-  _createClass(List, [{
-    key: "add",
-    value: function add(data) {
-      this.data.push(data);
-      console.log(this.data);
-    }
-  }]);
-
-  return List;
-}();
-
-var TodoList =
-/*#__PURE__*/
-function (_List) {
-  _inherits(TodoList, _List);
-
-  function TodoList() {
-    var _this;
-
-    _classCallCheck(this, TodoList);
-
-    _this = _possibleConstructorReturn(this, _getPrototypeOf(TodoList).call(this));
-    _this.usuario = "pedro";
-    return _this;
-  }
-
-  _createClass(TodoList, [{
-    key: "mostraUsuario",
-    value: function mostraUsuario() {
-      console.log(this.usuario);
-    }
-  }]);
-
-  return TodoList;
-}(List);
-
-var MinhaLista = new TodoList();
-
-document.getElementById("novotodo").onclick = function () {
-  MinhaLista.add("asdasdasas");
 };
+var nome = empresa.nome,
+    _empresa$endereco = empresa.endereco,
+    cidade = _empresa$endereco.cidade,
+    estado = _empresa$endereco.estado;
+console.log(nome); // Rocketseat
 
-MinhaLista.mostraUsuario();
+console.log(cidade); // Rio do Sul
+
+console.log(estado); // SC
+
+function mostraInfo(usuario) {
+  return "".concat(usuario.nome, " tem ").concat(usuario.idade, " anos.");
+}
+
+mostraInfo({
+  nome: 'Diego',
+  idade: 23
+});
--- curso-es6/main.js
+++ curso-es6/main.js
@@ -1,28 +1,49 @@
-class List {
-  constructor(){
-    this.data = []
-  }
 
-  add(data) {
-    this.data.push(data);
-    console.log(this.data);
+// 3.1
+
+// const arr = [1, 2, 3, 4, 5];
+
+// const teste = arr.map(item => item + 10);
+
+// console.log(teste);
+// 3.2 
+// Dica: Utilize uma constante pra function
+
+// const usuario = { nome: 'Diego', idade: 23 };
+
+// const mostraIdade = () => console.log(usuario.idade)
+
+// mostraIdade(usuario);
+// 3.3
+// Dica: Utilize uma constante pra function
+
+// const nome = "pedro";
+// const idade = 23;
+
+// const mostraUsuario = (nome = 'Diego', idade = 18) => (console.log({ nome, idade }));
+
+// mostraUsuario(nome, idade);
+// mostraUsuario(nome);
+// 3.4
+
+// const promise = () => new Promise( (resolve, reject) => resolve())
+// console.log(promise);
+
+const empresa = {
+  nome: 'Rocketseat',
+  endereco: {
+    cidade: 'Rio do Sul',
+    estado: 'SC',
   }
+};
+const {nome, endereco: {cidade, estado}} = empresa
+console.log(nome); // Rocketseat
+console.log(cidade); // Rio do Sul
+console.log(estado); // SC
+
+
+function mostraInfo(usuario) {
+  return `${usuario.nome} tem ${usuario.idade} anos.`;
 }
 
-class TodoList extends List {
-  constructor() {
-    super();
-    this.usuario = "pedro";
-  }
-
-  mostraUsuario() {
-    console.log(this.usuario);
-  }
-}
-const MinhaLista = new TodoList()
-
-document.getElementById("novotodo").onclick = function() {
-  MinhaLista.add("asdasdasas");
-}
-
-MinhaLista.mostraUsuario();
+mostraInfo({ nome: 'Diego', idade: 23 })
